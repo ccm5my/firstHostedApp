5878ea138798bdf31eae35f662a66ad5b9af4be6
Cameron Moore <cameronmoore@Camerons-MacBook-Air.local>
2023-08-13 10:04:07 -0400

    fixed application.properties

--- src/main/java/com/cameron/client/MortgageAPIClient.java
+++ src/main/java/com/cameron/client/MortgageAPIClient.java
@@ -3,15 +3,19 @@ package com.cameron.client;
 import com.cameron.models.*;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.beans.factory.annotation.Value;
 
 import com.cameron.interfaces.IMortgageAPIClient;
 
 @Component
 public class MortgageAPIClient implements IMortgageAPIClient {
 
+    @Value("${partnerId}")
+    private String partnerId;
+
     @Override
     public RateResponse getRates() {
-        String apiUrl = "https://mortgageapi.zillow.com/getCurrentRates?partnerId=RD-ZJDYSFZ";
+        String apiUrl = "https://mortgageapi.zillow.com/getCurrentRates?partnerId=" + partnerId;
         RestTemplate restTemplate = new RestTemplate();
         return restTemplate.getForObject(apiUrl, RateResponse.class);
     }
